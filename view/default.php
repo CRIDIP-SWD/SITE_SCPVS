1bb4b0ddbd5775240efcabe0b330c83ec6f6399c
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-05 15:12:04 +0100

    COMMIT SCPVS

--- view/default.php
+++ view/default.php
@@ -51,8 +51,8 @@
                 <!-- Logo
                 ============================================= -->
                 <div id="logo">
-                    <a href="index.html" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
-                    <a href="index.html" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+                    <a href="index.php?view=index" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="<?= $constante->getUrl(array(), false, true); ?>logo/scpvs.png" alt="Canvas Logo"></a>
+                    <a href="index.php?view=index" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="<?= $constante->getUrl(array(), false, true); ?>logo/scpvs.png" alt="Canvas Logo"></a>
                 </div><!-- #logo end -->
 
                 <!-- Primary Navigation
