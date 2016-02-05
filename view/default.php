62142ce188eadc7df9e8908263d59bdc7c56980d
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-05 12:47:12 +0100

    COMMIT SCPVS

--- view/default.php
+++ view/default.php
@@ -79,34 +79,11 @@
 
         <div class="swiper-container swiper-parent">
             <div class="swiper-wrapper">
-                <div class="swiper-slide dark" style="background-image: url('images/slider/swiper/1.jpg');">
+                <div class="swiper-slide dark" style="background-image: url('<?php $constante->getUrl(array(), false, true); ?>slide/slide_1.jpg');">
                     <div class="container clearfix">
                         <div class="slider-caption slider-caption-center">
-                            <h2 data-caption-animate="fadeInUp">Welcome to Canvas</h2>
-                            <p data-caption-animate="fadeInUp" data-caption-delay="200">Create just what you need for your Perfect Website. Choose from a wide range of Elements &amp; simply put them on our Canvas.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="swiper-slide dark">
-                    <div class="container clearfix">
-                        <div class="slider-caption slider-caption-center">
-                            <h2 data-caption-animate="fadeInUp">Beautifully Flexible</h2>
-                            <p data-caption-animate="fadeInUp" data-caption-delay="200">Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Powerful Layout with Responsive functionality that can be adapted to any screen size.</p>
-                        </div>
-                    </div>
-                    <div class="video-wrap">
-                        <video poster="images/videos/explore.jpg" preload="auto" loop autoplay muted>
-                            <source src='images/videos/explore.mp4' type='video/mp4' />
-                            <source src='images/videos/explore.webm' type='video/webm' />
-                        </video>
-                        <div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
-                    </div>
-                </div>
-                <div class="swiper-slide" style="background-image: url('images/slider/swiper/3.jpg'); background-position: center top;">
-                    <div class="container clearfix">
-                        <div class="slider-caption">
-                            <h2 data-caption-animate="fadeInUp">Great Performance</h2>
-                            <p data-caption-animate="fadeInUp" data-caption-delay="200">You'll be surprised to see the Final Results of your Creation &amp; would crave for more.</p>
+                            <h2 data-caption-animate="fadeInUp">Bienvenue sur le site de <strong>SCPVS</strong></h2>
+                            <p data-caption-animate="fadeInUp" data-caption-delay="200"><strong>S</strong>olutions <strong>C</strong>hirurgies <strong>P</strong>athologies <strong>V</strong>itale <strong>S</strong>anté</p>
                         </div>
                     </div>
                 </div>
