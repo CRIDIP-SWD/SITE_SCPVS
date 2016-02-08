bab8b466d0892468ba44fd74ba3d88acbebda7be
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-08 02:56:11 +0100

    COMMIT SCPVS

--- view/blog.php
+++ view/blog.php
@@ -5,6 +5,145 @@
 <?php if(isset($_GET['sub']) && $_GET['sub'] == 'article'): ?>
     <?php
     $ref = $_GET['ref'];
-    $article =
+    $article = $DB->query("SELECT * FROM blog WHERE idarticle = :ref", array("ref" => $ref));
     ?>
+
+<!-- Page Title
+    ============================================= -->
+<section id="page-title">
+
+    <div class="container clearfix">
+        <h1>Blog Single</h1>
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li class="active">Blog Single</li>
+        </ol>
+    </div>
+
+</section><!-- #page-title end -->
+
+<!-- Content
+============================================= -->
+<section id="content">
+
+    <div class="content-wrap">
+
+        <div class="container clearfix">
+
+            <div class="single-post nobottommargin">
+
+                <!-- Single Post
+                ============================================= -->
+                <div class="entry clearfix">
+
+                    <!-- Entry Title
+                    ============================================= -->
+                    <div class="entry-title">
+                        <h2>This is a Standard post with a Preview Image</h2>
+                    </div><!-- .entry-title end -->
+
+                    <!-- Entry Meta
+                    ============================================= -->
+                    <ul class="entry-meta clearfix">
+                        <li><i class="icon-calendar3"></i> 10th July 2014</li>
+                        <li><a href="#"><i class="icon-user"></i> admin</a></li>
+                        <li><i class="icon-folder-open"></i> <a href="#">General</a>, <a href="#">Media</a></li>
+                        <li><a href="#"><i class="icon-comments"></i> 43 Comments</a></li>
+                        <li><a href="#"><i class="icon-camera-retro"></i></a></li>
+                    </ul><!-- .entry-meta end -->
+
+                    <!-- Entry Image
+                    ============================================= -->
+                    <div class="entry-image bottommargin">
+                        <a href="#"><img src="images/blog/full/10.jpg" alt="Blog Single"></a>
+                    </div><!-- .entry-image end -->
+
+                    <!-- Entry Content
+                    ============================================= -->
+                    <div class="entry-content notopmargin">
+
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+
+                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. <a href="#">Curabitur blandit tempus porttitor</a>. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper.</p>
+
+                        <blockquote><p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p></blockquote>
+
+                        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus.</p>
+
+                        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. <a href="#">Nullam quis risus eget urna</a> mollis ornare vel eu leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+
+<pre>
+#header-inner {
+width: 940px;
+margin: 0 auto;
+padding-top: 40px;
+}</pre>
+
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+
+                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper.</p>
+                        <!-- Post Single - Content End -->
+
+                        <!-- Tag Cloud
+                        ============================================= -->
+                        <div class="tagcloud clearfix bottommargin">
+                            <a href="#">general</a>
+                            <a href="#">information</a>
+                            <a href="#">media</a>
+                            <a href="#">press</a>
+                            <a href="#">gallery</a>
+                            <a href="#">illustration</a>
+                        </div><!-- .tagcloud end -->
+
+                        <div class="clear"></div>
+
+                        <!-- Post Single - Share
+                        ============================================= -->
+                        <div class="si-share noborder clearfix">
+                            <span>Share this Post:</span>
+                            <div>
+                                <a href="#" class="social-icon si-borderless si-facebook">
+                                    <i class="icon-facebook"></i>
+                                    <i class="icon-facebook"></i>
+                                </a>
+                                <a href="#" class="social-icon si-borderless si-twitter">
+                                    <i class="icon-twitter"></i>
+                                    <i class="icon-twitter"></i>
+                                </a>
+                                <a href="#" class="social-icon si-borderless si-pinterest">
+                                    <i class="icon-pinterest"></i>
+                                    <i class="icon-pinterest"></i>
+                                </a>
+                                <a href="#" class="social-icon si-borderless si-gplus">
+                                    <i class="icon-gplus"></i>
+                                    <i class="icon-gplus"></i>
+                                </a>
+                                <a href="#" class="social-icon si-borderless si-rss">
+                                    <i class="icon-rss"></i>
+                                    <i class="icon-rss"></i>
+                                </a>
+                                <a href="#" class="social-icon si-borderless si-email3">
+                                    <i class="icon-email3"></i>
+                                    <i class="icon-email3"></i>
+                                </a>
+                            </div>
+                        </div><!-- Post Single - Share End -->
+
+                    </div>
+                </div><!-- .entry end -->
+
+                <!-- Post Navigation
+                ============================================= -->
+                <div class="post-navigation clearfix">
+
+                    <div class="col_half nobottommargin">
+                        <a href="#">&lArr; This is a Standard post with a Slider Gallery</a>
+                    </div>
+
+                    <div class="col_half col_last tright nobottommargin">
+                        <a href="#">This is an Embedded Audio Post &rArr;</a>
+                    </div>
+
+                </div><!-- .post-navigation end -->
 <?php endif; ?>
