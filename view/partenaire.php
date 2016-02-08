a8f2ec0997e1da86cf981d6c707ba177387a4251
CRIDIP-SWD <mmockelyn@cridip.com>
2016-02-08 03:18:51 +0100

    COMMIT SCPVS

--- view/partenaire.php
+++ view/partenaire.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Maxime
- * Date: 05/02/2016
- * Time: 15:47
- */
+<?php if(isset($_GET['sub']) && $_GET['sub'] == 'partenaire'): ?>
+    <div class="col_full nobottommargin clearfix">
+
+        <h3>6 Columns</h3>
+
+        <div class="masonry-thumbs col-6" data-big="3" data-lightbox="gallery">
+            <a href="assets/images/portfolio/full/1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/1.jpg" alt="Gallery Thumb 1"></a>
+            <a href="assets/images/portfolio/full/2.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/2.jpg" alt="Gallery Thumb 2"></a>
+            <a href="assets/images/portfolio/full/3.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/3.jpg" alt="Gallery Thumb 3"></a>
+            <a href="assets/images/portfolio/full/4.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/4.jpg" alt="Gallery Thumb 4"></a>
+            <a href="assets/images/portfolio/full/5.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/5.jpg" alt="Gallery Thumb 5"></a>
+            <a href="assets/images/portfolio/full/6.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/6.jpg" alt="Gallery Thumb 6"></a>
+            <a href="assets/images/portfolio/full/7.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/7.jpg" alt="Gallery Thumb 7"></a>
+            <a href="assets/images/portfolio/full/9.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/9.jpg" alt="Gallery Thumb 9"></a>
+            <a href="assets/images/portfolio/full/10.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/10.jpg" alt="Gallery Thumb 10"></a>
+            <a href="assets/images/portfolio/full/11.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/11.jpg" alt="Gallery Thumb 14"></a>
+            <a href="assets/images/portfolio/full/12.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/12.jpg" alt="Gallery Thumb 12"></a>
+            <a href="assets/images/portfolio/full/8.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/8.jpg" alt="Gallery Thumb 13"></a>
+            <a href="assets/images/portfolio/full/12-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/12-1.jpg" alt="Gallery Thumb 15"></a>
+            <a href="assets/images/portfolio/full/6-1.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/6-1.jpg" alt="Gallery Thumb 16"></a>
+            <a href="assets/images/portfolio/full/6-2.jpg" data-lightbox="gallery-item"><img class="image_fade" src="assets/images/portfolio/4/6-2.jpg" alt="Gallery Thumb 12-1"></a>
+        </div>
+
+    </div>
+<?php endif; ?>
